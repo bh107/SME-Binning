efadd8f40a506a5ed9bba7a9090352bc4b389dd8
Carl Johnsen <cjjohnsen@nbi.ku.dk>
2019-08-11 22:03:28 +0200

    Rewrote some of the buses

--- SME_Binning/Busses.cs
+++ SME_Binning/Busses.cs
@@ -4,7 +4,7 @@ using SME.VHDL;
 
 namespace SME_Binning
 {
-	
+
 	[InitializedBus]
 	public interface AdderIn : IBus
 	{
@@ -17,35 +17,20 @@ namespace SME_Binning
 		uint din { get; set; }
 	}
 
-	[InitializedBus, TopLevelInputBus]
-	public interface AXIBRAM0In : IBus
-	{
-		bool ena { get; set; }
-		UInt14 addr { get; set; }
-		uint din { get; set; }
-		UInt4 we { get; set; }
-	}
+    [InitializedBus]
+    public interface BRAMCtrl : IBus
+    {
+        bool ena { get; set; }
+        uint addr { get; set; }
+        bool wrena { get; set; }
+        uint wrdata { get; set; }
+    }
 
-	[InitializedBus, TopLevelOutputBus]
-	public interface AXIBRAM0Out : IBus
-	{
-		uint dout { get; set; }
-	}
-
-	[InitializedBus, TopLevelInputBus]
-	public interface AXIBRAM1In : IBus
-	{
-		bool ena { get; set; }
-		short addr { get; set; }
-		uint din { get; set; }
-		UInt4 we { get; set; }
-	}
-
-	[InitializedBus, TopLevelOutputBus]
-	public interface AXIBRAM1Out : IBus
-	{
-		uint dout { get; set; }
-	}
+    [InitializedBus]
+    public interface BRAMResult : IBus
+    {
+        uint rddata { get; set; }
+    }
 
 	[InitializedBus, TopLevelInputBus]
 	public interface AXIInput : IBus
@@ -59,123 +44,6 @@ namespace SME_Binning
 	public interface AXIOutput : IBus
 	{
 		uint outputrdy { get; set; }
-	}
-
-	[InitializedBus, TopLevelOutputBus]
-	public interface BRAM0AIn : IBus
-	{
-		bool ena { get; set; }
-		UInt12 addr { get; set; }
-		uint din { get; set; }
-		UInt4 we { get; set; }
-	}
-
-	[InitializedBus, TopLevelInputBus]
-	public interface BRAM0AOut : IBus
-	{
-		uint dout { get; set; }
-	}
-
-	[InitializedBus, TopLevelOutputBus]
-	public interface BRAM0BIn : IBus
-	{
-		bool ena { get; set; }
-		UInt12 addr { get; set; }
-		uint din { get; set; }
-		UInt4 we { get; set; }
-	}
-
-	[InitializedBus]
-	public interface BRAM0AInIntermediate : IBus
-	{
-		bool ena { get; set; }
-		UInt12 addr { get; set; }
-		uint din { get; set; }
-		UInt4 we { get; set; }
-	}
-
-	[InitializedBus]
-	public interface BRAM0AOutIntermediate : IBus
-	{
-		uint dout { get; set; }
-	}
-
-	[InitializedBus]
-	public interface BRAM0AOutPipelinedIn : IBus
-	{
-		uint dout { get; set; }
-	}
-
-	[InitializedBus]
-	public interface BRAM0AOutPipelinedOut : IBus
-	{
-		uint dout { get; set; }
-	}
-
-	[InitializedBus, TopLevelInputBus]
-	public interface BRAM0BOut : IBus
-	{
-		uint dout { get; set; }
-	}
-
-	[InitializedBus]
-	public interface BRAM0BOutPipelinedOut : IBus
-	{
-		uint dout { get; set; }
-	}
-
-	[InitializedBus, TopLevelOutputBus]
-	public interface BRAM1AIn : IBus
-	{
-		bool ena { get; set; }
-		UInt14 addr { get; set; }
-		uint din { get; set; }
-		UInt4 we { get; set; }
-	}
-
-	[InitializedBus]
-	public interface BRAM1AInBroadcasterIn : IBus
-	{
-		bool ena { get; set; }
-		UInt14 addr { get; set; }
-		uint din { get; set; }
-		UInt4 we { get; set; }
-	}
-
-	[InitializedBus]
-	public interface BRAM1AInBroadcasterOut : IBus
-	{
-		bool ena { get; set; }
-		UInt14 addr { get; set; }
-		uint din { get; set; }
-		UInt4 we { get; set; }
-	}
-
-	[InitializedBus]
-	public interface BRAM1AForwarded : IBus
-	{
-		uint dout { get; set; }
-	}
-
-	[InitializedBus, TopLevelInputBus]
-	public interface BRAM1AOut : IBus
-	{
-		uint dout { get; set; }
-	}
-
-	[InitializedBus, TopLevelOutputBus]
-	public interface BRAM1BIn : IBus
-	{
-		bool ena { get; set; }
-		UInt14 addr { get; set; }
-		uint din { get; set; }
-		UInt4 we { get; set; }
-	}
-
-	[InitializedBus, TopLevelInputBus]
-	public interface BRAM1BOut : IBus
-	{
-		uint dout { get; set; }
 	}
 
 	[InitializedBus]
